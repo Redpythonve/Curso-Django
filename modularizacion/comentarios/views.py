dd290cac7d9447a855f00ef63a9c2fa9bde0319f
Red Python <cornesalesve@gmail.com>
2024-12-18 07:12:23 -0400

    Add app-urls

--- modularizacion/comentarios/views.py
+++ modularizacion/comentarios/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def test(request):
+    return HttpResponse("funciona correctamente")
+
+def create(request):
+    return HttpResponse("funciona correctamente")
